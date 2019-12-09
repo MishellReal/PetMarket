474020980bf33c9f4bccac86c31a0a93a93cee7d
MishellReal <58490231+MishellReal@users.noreply.github.com>
2019-12-09 11:47:18 -0500

    commit

--- app/src/main/java/com/example/deber04/CreateAccount.kt
+++ app/src/main/java/com/example/deber04/CreateAccount.kt
@@ -2,11 +2,73 @@ package com.example.deber04
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
 
 class CreateAccount : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_account)
+
+        fun Ingresar(view: View){
+            var db : DatabaseHandler= DatabaseHandler(this)
+            var nombre : EditText = findViewById(R.id.editTextNombreRegistro)
+            var email : EditText = findViewById(R.id.editTextEmailRegistro)
+            var pw : EditText = findViewById(R.id.editTextEmailRegistro)
+
+            if(nombre.text.toString().isEmpty()){
+
+                nombre.setError("Ingresar nombre ")
+                return
+            }
+
+            if(email.text.toString().isEmpty()){
+                email.setError("Ingresar teléfono ")
+                return
+            }
+
+            if(pw.text.toString().isEmpty()){
+                pw.setError("Ingresar contraseña")
+                return
+            }
+
+            else{
+
+                db.addAccount(nombre.text.toString(), email.text.toString(), pw.text.toString())
+                var toast = Toast.makeText(applicationContext,"Registro Creado ${nombre.text.toString()}",
+                    Toast.LENGTH_LONG)
+                toast.show()
+                nombre.setText("")
+                nombre.setText("")
+
+            }
+
+
+
+        }
+
+        /*
+        else{
+
+            db.addContact(nombre_contacto.text.toString(), numero_contacto.text.toString())
+            var toast = Toast.makeText(applicationContext,"Registro Creado ${nombre_contacto.text.toString()}",
+                Toast.LENGTH_LONG)
+            toast.show()
+            nombre_contacto.setText("")
+            numero_contacto.setText("")
+
+        }
+
+    }
+
+    fun cancelar(view: View){
+
+        var regresar : Intent = Intent(this, MainActivity::class.java)
+        startActivity(regresar)
+
+    }
+}*/
     }
 }
--- app/src/main/java/com/example/deber04/Register.kt
+++ app/src/main/java/com/example/deber04/Register.kt
@@ -11,7 +11,7 @@ class Register : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
 
-        val button : Button = findViewById(R.id.buttonAceptar)
+        val button : Button = findViewById(R.id.buttonAceptarRegistro)
         button.setOnClickListener {
             var prIntent : Intent = Intent(this,Profile::class.java)
             startActivity(prIntent)
