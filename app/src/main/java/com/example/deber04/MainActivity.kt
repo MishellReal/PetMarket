79cff1ed17041eadc8fdaab835288590e99d3acc
MishellReal <58490231+MishellReal@users.noreply.github.com>
2019-12-09 10:27:11 -0500

    commit

--- app/src/main/java/com/example/deber04/MainActivity.kt
+++ app/src/main/java/com/example/deber04/MainActivity.kt
@@ -12,38 +12,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
-        val button: Button = findViewById(R.id.ButtonIngresar)
-        button.setOnClickListener {
-            val prIntent: Intent = Intent(this,IngresarActivity::class.java)
-            startActivity(prIntent)
-
-        }
-        val button2: Button = findViewById(R.id.buttonModificar)
-        button2.setOnClickListener {
-            val prIntent: Intent = Intent(this,ModificarActivity::class.java)
-            startActivity(prIntent)
-        }
-
-        val button3 : Button = findViewById(R.id.buttonEliminar)
-        button3.setOnClickListener {
-            val prIntent: Intent = Intent(this,EliminarActivity::class.java)
-            startActivity(prIntent)
-        }
-
-
-        val button4 : Button = findViewById(R.id.buttonConsultarId)
-        button4.setOnClickListener {
-            val prIntent: Intent = Intent(this,ConsultarTodoActivity::class.java)
-            startActivity(prIntent)
-        }
-
-        val button5 : Button = findViewById(R.id.buttonAllContacts)
-        button5.setOnClickListener {
-            val prIntent: Intent = Intent(this,ConsultarID::class.java)
-            startActivity(prIntent)
-        }
-
-
         val buttonlogin : Button = findViewById(R.id.buttonLogin)
         buttonlogin.setOnClickListener {
             val prIntent : Intent = Intent(this,Login::class.java)
