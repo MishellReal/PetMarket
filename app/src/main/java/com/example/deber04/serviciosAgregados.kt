979f2dc65cd2bd1142f2d749d3db66e3938a4e43
Emi Atiencie <58490441+remilio11@users.noreply.github.com>
2020-01-13 13:50:17 -0500

    crear actividad servicios agregados

---
+++ app/src/main/java/com/example/deber04/serviciosAgregados.kt
@@ -0,0 +1,12 @@
+package com.example.deber04
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class serviciosAgregados : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_servicios_agregados)
+    }
+}
