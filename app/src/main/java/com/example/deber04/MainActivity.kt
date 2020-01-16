196c3421f521a8d7a2accbe8f1b4cd2171062a0c
MishellReal <58490231+mishellreal@users.noreply.github.com>
2020-01-15 22:59:38 -0500

    evitar que existan campos vacios al iniciar sesion

--- app/src/main/java/com/example/deber04/MainActivity.kt
+++ app/src/main/java/com/example/deber04/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,8 +15,24 @@ class MainActivity : AppCompatActivity() {
 
         val buttonlogin: Button = findViewById(R.id.buttonLogin)
         buttonlogin.setOnClickListener {
-            val prIntent: Intent = Intent(this, Services::class.java)
-            startActivity(prIntent)
+
+            var nombre : EditText = findViewById(R.id.editTextUserLogin)
+            var pw : EditText = findViewById(R.id.editTextPwLogin)
+
+            if(nombre.text.toString().isEmpty()){
+
+                nombre.setError("Ingresar usuario ")
+            }
+
+            if(pw.text.toString().isEmpty()){
+
+                pw.setError("Ingresar contraseña ")
+            }
+            else {
+                val prIntent: Intent = Intent(this, Services::class.java)
+                startActivity(prIntent)
+            }
+
         }
 
         val buttonCrearCuenta: Button = findViewById(R.id.buttonCrearCuenta)
