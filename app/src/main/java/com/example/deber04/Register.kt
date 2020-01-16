7cf81b67c60c23b879dc93788274bd2b5d5191e2
MishellReal <mishell.real@epn.edu.ec>
2020-01-15 23:34:50 -0500

    evitar que existan campos vacios en registro de mascota

--- app/src/main/java/com/example/deber04/Register.kt
+++ app/src/main/java/com/example/deber04/Register.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 
 class Register : AppCompatActivity() {
 
@@ -13,8 +14,26 @@ class Register : AppCompatActivity() {
 
         val button : Button = findViewById(R.id.buttonAceptarRegistro)
         button.setOnClickListener {
+            var nombre : EditText = findViewById(R.id.editTextNameMascota)
+            var edad : EditText = findViewById(R.id.editTextEdadMascota)
+            var desc : EditText = findViewById(R.id.editTextDescripcion)
+
+            if (nombre.text.toString().isEmpty()){
+                nombre.setError("Ingresar nombre de mascota ")
+            }
+
+            if (edad.text.toString().isEmpty()){
+                edad.setError("Ingresar edad de mascota ")
+            }
+
+            if (desc.text.toString().isEmpty()){
+                desc.setError("Ingresar descripción de mascota ")
+            }
+
+            else{
             var prIntent : Intent = Intent(this,Services::class.java)
             startActivity(prIntent)
+        }
 
         }
     }
