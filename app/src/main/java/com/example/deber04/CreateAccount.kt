308a4658868672e3013236dfe932ff6807e205e3
MishellReal <mishell.real@epn.edu.ec>
2020-01-15 23:17:27 -0500

    evitar que existan campos vacios en registro de usuario

--- app/src/main/java/com/example/deber04/CreateAccount.kt
+++ app/src/main/java/com/example/deber04/CreateAccount.kt
@@ -16,10 +16,42 @@ class CreateAccount : AppCompatActivity() {
 
         val buttonAceptarRegistro: Button = findViewById(R.id.buttonAceptarRegistro)
         buttonAceptarRegistro.setOnClickListener {
+
+            var nombre : EditText = findViewById(R.id.editTextNombreReg)
+            var apellido : EditText = findViewById(R.id.editTextApellidoReg)
+            var email : EditText = findViewById(R.id.editTextEmailReg)
+            var phone : EditText = findViewById(R.id.editTextPhoneReg)
+            var pw : EditText = findViewById(R.id.editTextPwReg)
+            var confPw : EditText = findViewById(R.id.editTextConfirmaPwReg)
+
+            if (nombre.text.toString().isEmpty()){
+                nombre.setError("Ingresar nombre de usuario ")
+            }
+
+            if (apellido.text.toString().isEmpty()){
+                apellido.setError("Ingresar apellido de usuario ")
+            }
+
+            if (email.text.toString().isEmpty()){
+                email.setError("Ingresar correo electrónico de usuario ")
+            }
+
+            if (phone.text.toString().isEmpty()){
+                phone.setError("Ingresar teléfono de usuario ")
+            }
+
+            if (pw.text.toString().isEmpty()){
+                pw.setError("Ingresar contraseña de usuario ")
+            }
+
+            if (confPw.text.toString().isEmpty()){
+                confPw.setError("Confirmar contraseña de usuario ")
+            }
+
+            else {
             val prIntent: Intent = Intent(this, Register::class.java)
             startActivity(prIntent)
-
-
+        }
         }
 
 
