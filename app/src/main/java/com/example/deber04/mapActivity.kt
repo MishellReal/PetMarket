961e85566e8221be1f9cd709d975ebc316d8d9ea
MishellReal <mishell.real@epn.edu.ec>
2020-01-16 11:28:06 -0500

    configuracion inicial para firebase

---
+++ app/src/main/java/com/example/deber04/mapActivity.kt
@@ -0,0 +1,18 @@
+package com.example.deber04
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class mapActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_map)
+
+        /*val streetViewPanoramaFragment = fragmentManager
+            .findFragmentById(R.id.streetviewpanorama) as StreetViewPanoramaFragment
+        streetViewPanoramaFragment.getStreetViewPanoramaAsync(this)*/
+    }
+
+
+}
