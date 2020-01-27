2cb4968d8fe9afee579bead1590701a43f81c939
MishellReal <mishell.real@epn.edu.ec>
2020-01-27 10:58:50 -0500

    dar funcionalidad a los botones de metodo de pago

--- app/src/main/java/com/example/deber04/metodoPagoActivity.kt
+++ app/src/main/java/com/example/deber04/metodoPagoActivity.kt
@@ -1,12 +1,28 @@
 package com.example.deber04
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class metodoPagoActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_metodo_pago)
+
+
+        val button: Button = findViewById(R.id.buttonTarjeta)
+        button.setOnClickListener {
+            var prIntent: Intent = Intent(this, metodoPagoActivity::class.java)
+            startActivity(prIntent)
+
+            val button: Button = findViewById(R.id.buttonEfectivo)
+            button.setOnClickListener {
+                var prIntent: Intent = Intent(this, metodoPagoActivity::class.java)
+                startActivity(prIntent)
+
+            }
+        }
     }
 }
