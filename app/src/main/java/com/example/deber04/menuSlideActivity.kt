013e0b6aa98a4053c4814be0d35f5035bbf8aba4
MishellReal <mishell.real@epn.edu.ec>
2020-02-08 22:51:19 -0500

    Modificar datos de mascota

--- app/src/main/java/com/example/deber04/menuSlideActivity.kt
+++ app/src/main/java/com/example/deber04/menuSlideActivity.kt
@@ -44,6 +44,8 @@ class menuSlideActivity : AppCompatActivity() {
         navView.setupWithNavController(navController)
     }
 
+
+
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_slide, menu)
